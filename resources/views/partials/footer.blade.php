372259c9851cba1c5565ff568217d2676a5e34f4
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2022-02-02 18:14:00 +0100

    Fine footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,51 @@
 <footer>
-    
+    <div class="footer-top">
+        <div class="footer-top-container">
+            <div class="footer-top-links">
+                <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="Digital Comics">
+                <span>digital comics</span>
+            </div>
+            <div class="footer-top-links">
+                <img src="{{asset('img/buy-comics-merchandise.png')}}" alt="Merchandise">
+                <span>comics merchandise</span>
+            </div>
+            <div class="footer-top-links">
+                <img src="{{asset('img/buy-comics-subscriptions.png')}}" alt="Subscriptions">
+                <span>subscriptions</span>
+            </div>
+            <div class="footer-top-links">
+                <img src="{{asset('img/buy-comics-shop-locator.png')}}" alt="Location">
+                <span>comics shop location</span>
+            </div>
+            <div class="footer-top-links">
+                <img id="piuPiccola" src="{{asset('img/buy-dc-power-visa.svg')}}" alt="Visa">
+                <span>dc power visa</span>
+            </div>
+        </div>
+    </div>
+
+    <!-- Inizio footer-center -->
+    <div class="footer-center">
+        <div class="footer-center-container">
+            <div class="list">
+                <span class="white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam officia tenetur temporibus et, voluptas adipisci!</span>
+                <span class="gray">Lorem, ipsum dolor.</span>
+                <span class="white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam officia tenetur temporibus et, voluptas adipisci!</span>
+                <span class="gray">Lorem, ipsum dolor.</span>
+                <span class="white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam officia tenetur temporibus et, voluptas adipisci!</span>
+                <span class="gray">Lorem, ipsum dolor.</span>
+            </div>
+            <img src="{{asset('img/dc-logo-bg.png')}}">
+        </div>
+    </div>
+
+    <!-- Inizio footer-bottom -->
+    <div class="footer-bottom">
+        <div class="footer-bottom-container">
+            <div class="left">
+                Lorem, ipsum.
+            </div>
+            <div class="right">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, magnam?</div>
+        </div>
+    </div>
 </footer>
